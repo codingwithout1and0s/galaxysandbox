837bf2addc90afcad5a8e96e587ba1d01fd835d7
codingwithout1and0s <codingwithout1and0s@gmail.com>
2023-12-02 14:52:59 -0600

    First commit

---
+++ src/main/java/org/comit/GalaxySandbox/GalaxySandboxApplication.java
@@ -0,0 +1,13 @@
+package org.comit.GalaxySandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxySandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxySandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/comit/GalaxySandbox/GalaxySandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.comit.GalaxySandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxySandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
